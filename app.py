2333e77581ab88cc945fb6b87bc398a0746d7e91
billalxcode <billal.xcode@gmail.com>
2025-02-28 00:53:08 +0700

    feat: integrate Agent and runtime initialization in app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-from uuid import uuid4
+from dotenv import load_dotenv
 from pymongo import MongoClient
-from datetime import datetime
-from eth_account import Account
-from eth_account.signers.local import LocalAccount
+from src.core.agent import Agent
 from src.core.runtime import AgentRuntime
 from src.core.character import CharacterManager
 from src.core.settings import SettingsManager
 from src.adapters.mongodb import MongoAdapter
 
-from src.core.logger import console
-from src.core.types import Users
-from src.core.types import Wallet
+load_dotenv()
 
 character_manager = CharacterManager()
 settings_manager = SettingsManager()
@@ -19,33 +15,18 @@ runtime = AgentRuntime(
     character=character_manager.character,
     settings=settings_manager.settings
 )
+
+agent = Agent(runtime=runtime)
+agent.init()
+
+runtime.set_agent(agent=agent)
+runtime.init()
+
 database_client = MongoClient(uuidRepresentation="standard")
-database_adapter = MongoAdapter(settings_manager.settings, database_client)
+database_adapter = MongoAdapter(runtime, database_client)
 database_adapter.init()
 
 runtime.set_database_adapter(database_adapter)
 
-user_data = Users(
-    uuid=uuid4(),
-    fullname="Billal Fauzan",
-    username="billalxcode",
-    wallets=[],
-    created_at=str(datetime.now()),
-    updated_at=str(datetime.now())
-)
-
-secret_key = runtime.settings.secret_key
-account: LocalAccount = Account.create()
-wallet_data = Wallet(
-    uuid=uuid4(),
-    address=str(account.address),
-    encrypted_private_key="",
-    wallet_type="evm"
-)
-wallet_data.lock_private_key(secret_key, account.key.hex())
-database_adapter.create_wallet("23e7d4f0-686d-4d93-b96a-9e180779bacf", wallet_data)
-# console.print(user_data)
-# result = database_adapter.create_user(user_data=user_data)
-# console.print(result)
-# result = database_adapter.create_conversation(conversation_data=conversation_data)
-# console.print(result)
+database = runtime.get_setting("database.mongodb")
+runtime.start()
