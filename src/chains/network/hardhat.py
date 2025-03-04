9e8264bbb1595671101b55f53acb6f291b02bf7f
billalxcode <billal.xcode@gmail.com>
2025-03-04 22:21:26 +0700

    feat: enhance HardhatNetwork initialization with additional parameters and middleware

--- src/chains/network/hardhat.py
+++ src/chains/network/hardhat.py
@@ -3,5 +3,9 @@ from src.chains.base import BaseChainEvm
 class HardhatNetwork(BaseChainEvm):
     def __init__(self):
         super().__init__(
-            "http://localhost:8545"
+            "http://localhost:7545",
+            name="hardhat",
+            base_explorer_url="http://localhost:8545/"
         )
+        self.add_poa_middleware()
+        self.add_gas_price_strategy()
