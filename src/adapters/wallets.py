fffaab109e21af8ebfb8238a70d135ed7bcad94b
billalxcode <billal.xcode@gmail.com>
2025-03-04 22:21:01 +0700

    feat: add wallet retrieval and account decryption methods to WalletAdapter

--- src/adapters/wallets.py
+++ src/adapters/wallets.py
@@ -3,6 +3,8 @@ from src.core.logger import console
 from src.core.types import MongoAdapterAbstract
 from src.core.types import WalletAdapterAbstract
 from src.core.types import Wallet
+from src.core.types import Literal
+from src.core.types import LocalAccount
 from src.core.exceptions import InvalidID
 
 class WalletAdapter(WalletAdapterAbstract):
@@ -20,3 +22,35 @@ class WalletAdapter(WalletAdapterAbstract):
         except Exception as e:
             console.error(f"Error creating a user: {e}")
             return False
+        
+    def get_wallet(self, user_id: UUID, wallet_type: Literal["evm", "solana"]):
+        self.db.ensure_connection()
+
+        if isinstance(user_id, str):
+            try:
+                user_id = UUID(user_id)
+            except ValueError:
+                raise InvalidID("Invalid user id")
+        
+        if wallet_type not in ["evm", "solana"]:
+            raise Exception(f"Wallet type {wallet_type} not supported")
+        
+        try:
+            wallet = self.db.database.get_collection("wallets").find_one({
+                "user_uuid": user_id,
+                "wallet_type": wallet_type
+            })
+            if wallet:
+                return Wallet(**wallet)
+            return None
+        except Exception as e:
+            console.error(f"Error retrieving wallet for user {user_id}: {e}")
+            return None
+    
+    def get_wallet_account(self, user_id: UUID, wallet_type: Literal["evm", "solana"], secret: str) -> LocalAccount | None:
+        wallet = self.get_wallet(user_id=user_id, wallet_type=wallet_type)
+        if wallet is None: return None
+        
+        wallet_account = wallet.decrypt_extra(secret=secret)
+        return wallet_account
+    
