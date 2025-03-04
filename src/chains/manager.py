5b578bbd7b975ecbf9028c9e8a20e7ae7bb06205
billalxcode <billal.xcode@gmail.com>
2025-03-04 22:21:12 +0700

    feat: implement chain registration and selection in ChainsManager

--- src/chains/manager.py
+++ src/chains/manager.py
@@ -1,10 +1,23 @@
+from typing import Literal
+from src.core.logger import console
 from src.chains.types import ChainsManagerAbstract
+from src.chains.base import BaseChainEvm
+from src.chains.network.hardhat import HardhatNetwork
 
 class ChainsManager(ChainsManagerAbstract):
     def __init__(self):
         super().__init__()
 
-        self.chains = []
+        self.chains = {}
 
-    def register_chain(self):
-        return super().register_chain()
+    def register_chain(self, name: str, cls: BaseChainEvm):
+        console.info(f"Registering chain: {name}")
+        self.chains[name] = {
+            "cls": cls
+        }
+
+    def register_chains(self):
+        self.register_chain("hardhat", HardhatNetwork())
+        
+    def select_chain(self, name: Literal["ethereum", "base", "bsc", "hardhat"]):
+        return self.chains[name]["cls"]
